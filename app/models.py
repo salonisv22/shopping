b7c1fa943a4cff9a8f63261c312ffd85a373a5fe
salonisv22 <77237060+salonisv22@users.noreply.github.com>
2021-09-28 17:03:51 +0530

    product details doesn't work in filter

--- app/models.py
+++ app/models.py
@@ -34,9 +34,28 @@ class Product(models.Model):
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models,ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return str(self.id)
+
+STATE_CHOICES = (
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The Way','On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel')
+)
+
+class OrderPlaced(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    customer=models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+    ordered_date=models.DateTimeField(auto_now_add=True)
+    status=models.CharField(max_length=50,choices=STATE_CHOICES,default='Pending')
+
+
+
 
