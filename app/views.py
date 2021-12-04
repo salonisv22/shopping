3cee251bcec421629f873fb69469031cf04647f0
nikki1409 <nikita14jha@gmail.com>
2021-12-04 15:45:32 +0530

    till timestamp 4:55

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from .models import Customer, Cart, Product, OrderPlaced
-from .forms import CustomerRegistrationForm
+from .forms import CustomerRegistrationForm,CustomerProfileForm
 from django.contrib import messages
 # def home(request):
 
@@ -15,32 +15,45 @@ class ProductView(View):
         return render(request, 'app/home.html', {'topwears':topwears, 'bottomwears': bottomwears, 'mobiles':mobiles})
 
 
-# def product_detail(request): 
-
-#  return render(request, 'app/productdetail.html')
-
 class ProductDetailView(View):
     def get(self, request, pk):
         product = Product.objects.get(pk=pk)
         return render(request, 'app/productdetail.html', {'product':product})
 
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+    user=request.user
+    product_id=request.GET.get('prod_id')
+    product=Product.objects.get(id=product_id)
+    Cart(user=user,product=product).save()
+    return redirect('/cart')
+
+def show_cart(request):
+    if request.user.is_authenticated:
+        user=request.user
+        cart=Cart.objects.filter(user=user)
+        amount=0.0
+        shipping_amount=70.0
+        total_amount=0.0
+        cart_product=[p for p in Cart.objects.all() if p.user==user]
+        if cart_product:
+            for p in cart_product:
+                temp_amount=(p.quantity * p.product.discounted_price)
+                amount += temp_amount
+                total_amount=amount+shipping_amount
+            return render(request,'app/addtocart.html',{'carts':cart,'amount':amount,'totalamount':total_amount})
+        else:
+            return render(request,'app/emptycart.html')
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
 
-def profile(request):
- return render(request, 'app/profile.html')
-
 def address(request):
- return render(request, 'app/address.html')
+    add=Customer.objects.filter(user=request.user)
+    return render(request, 'app/address.html',{'add':add,'active':'btn-primary'})
 
 def orders(request):
  return render(request, 'app/orders.html')
 
-def change_password(request):
- return render(request, 'app/changepassword.html')
 
 def mobile(request, data=None ):
     if data == None:
@@ -70,3 +83,22 @@ class CustomerRegistrationView(View):
 
 def checkout(request):
     return render(request, 'app/checkout.html')
+
+
+class ProfileView(View):
+    def get(self,request):
+        form=CustomerProfileForm()
+        return render(request,'app/profile.html',{'form':form,'active':'btn-primary'})
+    def post(self,request):
+        form=CustomerProfileForm(request.POST)
+        if form.is_valid():
+            usr=request.user
+            name=form.cleaned_data['name']
+            locality=form.cleaned_data['locality']
+            city=form.cleaned_data['city']
+            state=form.cleaned_data['state']
+            zipcode=form.cleaned_data['zipcode']
+            reg=Customer(user=usr,name=name,locality=locality,city=city,state=state,zipcode=zipcode)
+            reg.save()
+            messages.success(request,'Profile Updated Successfully!!!')
+            return render(request,'app/profile.html',{'form':form,'active':'btn-primary'})
