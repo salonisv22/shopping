61beda0335809b1bf66592becb90f4b7d5c8ac37
salonisv22 <77237060+salonisv22@users.noreply.github.com>
2021-12-31 15:33:46 +0530

    removed statistical folder

--- shop/settings.py
+++ shop/settings.py
@@ -140,7 +140,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
