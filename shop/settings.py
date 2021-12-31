b96c3fd0c373de08e890e9e33db61230cf19cf61
salonisv22 <77237060+salonisv22@users.noreply.github.com>
2021-12-31 14:23:21 +0530

    added collect static root in settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,8 +125,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 LOGIN_REDIRECT_URL='/profile/'
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
-
+SESSION_ENGINE='django.contrib.sessions.backends.db'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
